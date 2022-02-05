1d3d9322cfe68933cffe74a3245f066b7c92dc2a
jiawei <jiaweizheng416528@gmail.com>
2022-02-05 11:38:57 -0800

    patched up queue and added tests to queue_tester

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -37,10 +37,149 @@ void test_queue_simple(void)
 	TEST_ASSERT(ptr == &data);
 }
 
+//enqueue/dequeue complex
+void test_queue_complex(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_complex ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_dequeue(q, (void**)&ptr);
+	queue_dequeue(q, (void**)&ptr2);
+	queue_dequeue(q, (void**)&data3);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr2 == &data2);
+	TEST_ASSERT(ptr3 == &data3);
+}
+
+//delete beginning
+void test_queue_delete_beg(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_beg ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_delete(q, &data);
+	queue_dequeue(q, (void**)&ptr2);
+	queue_dequeue(q, (void**)&ptr3);
+	TEST_ASSERT(ptr2 == &data2);
+	TEST_ASSERT(ptr3 == &data3);
+}
+
+//delete mid1
+void test_queue_delete_mid(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_mid ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_delete(q, &data2);
+	queue_dequeue(q, (void**)&ptr);
+	queue_dequeue(q, (void**)&ptr3);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr3 == &data3);
+}
+
+//delete mid2
+void test_queue_delete_mid2(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_mid ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_dequeue(q, (void**)&ptr);
+	queue_delete(q, &data2);
+	queue_dequeue(q, (void**)&ptr3);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr3 == &data3);
+}
+
+//delete end1
+void test_queue_delete_end(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_end ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_delete(q, &data3);
+	queue_dequeue(q, (void**)&ptr);
+	queue_dequeue(q, (void**)&ptr2);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr2 == &data2);
+}
+
+//delete end2
+void test_queue_delete_end(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_end ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_dequeue(q, (void**)&ptr);
+	queue_delete(q, &data3);
+	queue_dequeue(q, (void**)&ptr2);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr2 == &data2);
+}
+
+//delete end3
+void test_queue_delete_end(void)
+{
+	int data = 3, data2 = 4, data3 = 5, *ptr, *ptr2, *ptr3;
+	queue_t q;
+
+	printf("*** TEST queue_delete_end ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_enqueue(q, &data2);
+	queue_enqueue(q, &data3);
+	queue_dequeue(q, (void**)&ptr);
+	queue_dequeue(q, (void**)&ptr2);
+	queue_delete(q, &data3);
+	TEST_ASSERT(ptr == &data);
+	TEST_ASSERT(ptr2 == &data2);
+}
+
 int main(void)
 {
 	test_create();
 	test_queue_simple();
+	test_queue_complex();
+	test_queue_delete_beg();
+	test_queue_delete_mid();
+	test_queue_delete_mid2();
+	test_queue_delete_end();
 
 	return 0;
 }
