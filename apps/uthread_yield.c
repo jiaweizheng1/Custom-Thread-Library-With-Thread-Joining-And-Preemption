9acac2b96df95e03ff317209b4559806081ccc11
jiawei <jiaweizheng416528@gmail.com>
2022-02-07 20:50:24 -0800

    :)

--- apps/uthread_yield.c
+++ apps/uthread_yield.c
@@ -19,7 +19,7 @@ int thread3(void)
 {
 	uthread_yield();
 	printf("thread%d\n", uthread_self());
-	return 0;
+	return 69;
 }
 
 int thread2(void)
@@ -27,7 +27,7 @@ int thread2(void)
 	uthread_create(thread3);
 	uthread_yield();
 	printf("thread%d\n", uthread_self());
-	return 0;
+	return 21;
 }
 
 int thread1(void)
@@ -36,13 +36,14 @@ int thread1(void)
 	uthread_yield();
 	printf("thread%d\n", uthread_self());
 	uthread_yield();
-	return 0;
+	return 777;
 }
 
 int main(void)
 {
 	uthread_start(0);
 	uthread_join(uthread_create(thread1), NULL);
+	uthread_join(3, NULL);
 	uthread_stop();
 
 	printf("main exiting\n");
